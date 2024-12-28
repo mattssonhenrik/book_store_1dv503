18e22b5d74a22c93ceae6d1a10298f0707df9320
Henrik Mattsson <hm222yj@student.lnu.se>
2024-12-28 14:19:50 +0100

    Add Data transfer object for member to order.

--- app/src/main/java/book_store_1dv503/controller/OrderController.java
+++ app/src/main/java/book_store_1dv503/controller/OrderController.java
@@ -5,7 +5,11 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
+import book_store_1dv503.model.DTO_member_adress;
+import book_store_1dv503.model.Order;
+
 public class OrderController {
+  Order order;
   int orderNumber;
   int userId;
   String url = "jdbc:mysql://localhost:3306/book_store";
@@ -14,14 +18,50 @@ public class OrderController {
 
   public OrderController(int userId) {
     this.userId = userId;
-    orderNumber = getOrderNumber();
 
-    displayInvoice(orderNumber); 
-    storeOrderInDatabase();
+    registerOrder();
+
+    orderNumber = getOrderNumber();
+    displayInvoice(orderNumber);
   }
 
-  public int getOrderNumber () {
-        String query = "SELECT * FROM cart join books ON cart.isbn = books.isbn WHERE userId = " + '"' + userId + '"';
+    public void registerOrder() {
+    DTO_member_adress memberAddress = getMemberAddress();
+    String shipAddress = memberAddress.getAddress();
+    String shipCity = memberAddress.getCity();
+    int shipZip = memberAddress.getZip();
+    order = new Order(userId, shipAddress, shipCity, shipZip);
+    storeOrderInDatabase(order);
+  }
+
+  public DTO_member_adress getMemberAddress() {
+    String query = "SELECT * FROM members WHERE userId = " + '"' + userId + '"';
+    try (
+        Connection connection = DriverManager.getConnection(url, user, password);
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery(query)) {
+      if (resultSet.isBeforeFirst()) {
+        while (resultSet.next()) {
+          String address = resultSet.getString("address");
+          String city = resultSet.getString("city");
+          int zip = resultSet.getInt("zip");
+          DTO_member_adress dto_member_adress = new DTO_member_adress(address, city, zip);
+          return dto_member_adress;
+        }
+      }
+    } catch (Exception e) {
+      System.out.println("Database connection failed!");
+      e.printStackTrace();
+    }
+    return null;
+  }
+
+
+
+
+
+  public int getOrderNumber() {
+    String query = "SELECT * FROM orders join books ON cart.isbn = books.isbn WHERE userId = " + '"' + userId + '"';
     try (
         Connection connection = DriverManager.getConnection(url, user, password);
         Statement statement = connection.createStatement();
@@ -42,7 +82,7 @@ public class OrderController {
     System.out.println("This is your ordernumber " + orderNumber);
   }
 
-  public void storeOrderInDatabase() {
+  public void storeOrderInDatabase(Order order) {
 
     deleteCartForOrder();
   }
---
+++ app/src/main/java/book_store_1dv503/model/DTO_member_adress.java
@@ -0,0 +1,37 @@
+package book_store_1dv503.model;
+
+public class DTO_member_adress {
+  private String address;
+  private String city;
+  private int zip;
+
+  public DTO_member_adress(String address, String city, int zip) {
+    this.setAddress(address);
+    this.setCity(city);
+    this.setZip(zip);
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getCity() {
+    return city;
+  }
+
+  public int getZip () {
+    return zip;
+  }
+
+  public void setAddress (String address) {
+    this.address = address;
+  }
+
+  public void setCity (String city) {
+    this.city = city;
+  }
+
+  public void setZip (int zip) {
+    this.zip = zip;
+  }
+}
